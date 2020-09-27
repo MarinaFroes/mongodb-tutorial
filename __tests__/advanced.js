27761277699ab68f426b1d18eef433d3c5dc9ccb
MFAC <ninafroes@gmail.com>
2020-09-27 19:23:58 +0200

    test: Add geoNear test

--- __tests__/advanced.js
+++ __tests__/advanced.js
@@ -48,4 +48,70 @@ describe('advanced mongodb features', () => {
     expect(secondGroup[0].name).toBe('Company 51')
   })
 
+  test('geoNear', async () => {
+    const SFCoordinates = [ -122.4223791, 37.7679638 ]
+    const NYCoordinates = [ -73.9758597, 40.7830649 ]
+    const ClientNearbyCoordinates = [ -73.9795183, 40.784056 ]
+
+    let taxi = new Taxi()
+    taxi.brand = 'Toyota'
+    taxi.model = 'Yaris'
+    taxi.year = 2015
+    taxi.owner = { name: 'Driver 1', experience: 15 }
+    taxi.geometry = { coordinates: SFCoordinates }
+    taxi = await taxi.save()
+
+    let taxi2 = new Taxi()
+    taxi2.brand = 'Benz'
+    taxi2.model = 'Class E'
+    taxi2.year = 2017
+    taxi2.owner = { name: 'Driver 2', experience: 7 }
+    taxi2.geometry = { coordinates: NYCoordinates }
+    taxi2 = await taxi2.save()
+
+    const taxisNearbyOneKm = await Taxi.aggregate([
+      {
+        $geoNear: {
+          near: {
+            type: 'Point',
+            coordinates: ClientNearbyCoordinates
+          },
+          spherical: true,
+          maxDistance: 1 * 1000,
+          distanceField: 'dist.calculated'
+        }
+      }
+    ])
+    expect(taxisNearbyOneKm[0].owner.name).toBe('Driver 2')
+
+    const taxisNearbyQuarterKm = await Taxi.aggregate([
+      {
+        $geoNear: {
+          near: {
+            type: 'Point',
+            coordinates: ClientNearbyCoordinates
+          },
+          spherical: true,
+          maxDistance: 0.25 * 1000,
+          distanceField: 'dist.calculated'
+        }
+      }
+    ])
+    expect(taxisNearbyQuarterKm.length).toBe(0)
+
+    const taxisNearbyThousandsKm = await Taxi.aggregate([
+      {
+        $geoNear: {
+          near: {
+            type: 'Point',
+            coordinates: ClientNearbyCoordinates
+          },
+          spherical: true,
+          maxDistance: 5000 * 1000,
+          distanceField: 'dist.calculated'
+        }
+      }
+    ])
+    expect(taxisNearbyThousandsKm.length).toBe(2)
+  })
 })
