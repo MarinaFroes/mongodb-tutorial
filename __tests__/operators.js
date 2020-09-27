97bb8149fafc93536ad681c3e66a4d541c05cbe0
MFAC <ninafroes@gmail.com>
2020-09-27 11:12:58 +0200

    test: Add tests with $gt and $lt operators

---
+++ __tests__/operators.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose')
+const Company = require('../models/Company')
+const Taxi = require('../models/Taxi')
+
+beforeAll(() => {
+  mongoose.Promise = global.Promise
+  mongoose.connect('mongodb://localhost/taxi-aggregator', {
+    useNewUrlParser: true,
+    useCreateIndex: true
+  })
+})
+
+beforeEach(async () => {
+
+})
+
+afterEach(async () => {
+  await Company.deleteMany({})
+  await Taxi.deleteMany({})
+})
+
+afterAll(done => {
+  mongoose.disconnect(done)
+})
+
+describe('mongo operators tests', () => {
+
+  test('$gt and $lt', async () => {
+    for (let i = 1; i <= 5; i++){
+      let taxi = new Taxi()
+      taxi.brand = 'Toyota'
+      taxi.model = 'Yaris'
+      taxi.year = 2015
+      taxi.owner = {
+        name: `Driver ${i}`,
+        experience: 5 * i
+      }
+      await taxi.save()
+    }
+
+    const count = await Taxi.countDocuments()
+    expect(count).toBe(5)
+    const readTaxis = await Taxi.find({
+      'owner.experience': { $gt: 6, $lt: 21 }
+    })
+
+    expect(readTaxis.length).toBe(3)
+  })
+})
